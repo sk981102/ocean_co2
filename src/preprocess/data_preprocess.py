ccf73aa1a37b59f0f4e37e755b12753d1d9547e7
sk981102 <sk981102@gmail.com>
2021-12-10 14:53:05 -0500

    nfp!

--- src/preprocess/data_preprocess.py
+++ src/preprocess/data_preprocess.py
@@ -19,7 +19,7 @@ def preprocess_images(dir_name):
     
     pco2_images = preprocess_image_reduced(pco2.pCO2.data)
     
-    X = np.dstack((chl_images, mld_images, sss_images, sst_images, xco2_images), axis = 1)
+    X = np.dstack((chl_images, mld_images, sss_images, sst_images, xco2_images))
     X = X.reshape((421,180,360,5),order='F')
 
     return X, pco2_images
@@ -36,7 +36,7 @@ def preprocess_images_nfp(dir_name):
     
     pco2_images = preprocess_image_reduced(pco2.pCO2.data)
     
-    X = np.dstack((chl_images, mld_images, sss_images, sst_images, xco2_images,pco2_images), axis = 1)
+    X = np.dstack((chl_images, mld_images, sss_images, sst_images, xco2_images,pco2_images))
     X = X.reshape((421,180,360,6),order='F')
 
     return X, pco2_images
