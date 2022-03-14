ed934b9dfcc740ee882b9629b9295dcc2f9cff3c
sk981102 <sk981102@gmail.com>
2022-03-14 13:47:31 -0400

    0314

--- src/preprocess/data_preprocess.py
+++ src/preprocess/data_preprocess.py
@@ -7,40 +7,57 @@ sys.path.insert(0, '../src')
 
 from utils import df_to_xarray,read_xarray
 ### Image Data Preprocess for CNN ###
-def preprocess_images(dir_name):
+
+
+def preprocess_images(dir_name, num="001",socat=False):
+    chl,mld,sss,sst,u10,fg_co2,xco2,icefrac,patm,pco2 = read_xarray(dir_name,num)
     
-    chl,mld,sss,sst,u10,fg_co2,xco2,icefrac,patm,pco2 = read_xarray(dir_name)
+    if socat:
+
+        chl_images = preprocess_image_reduced(chl.Chl_socat.data)
+        mld_images = preprocess_image_reduced(mld.MLD_socat.data)
+        sss_images = preprocess_image_reduced(sss.SSS_socat.data)
+        sst_images = preprocess_image_reduced(sst.SST_socat.data)
+        xco2_images = preprocess_image_reduced(xco2.XCO2.data,xco2=True)
+        pco2_images = preprocess_image_reduced(pco2.pCO2_socat.data)
     
-    chl_images = preprocess_image_reduced(chl.Chl.data)
-    mld_images = preprocess_image_reduced(mld.MLD.data)
-    sss_images = preprocess_image_reduced(sss.SSS.data)
-    sst_images = preprocess_image_reduced(sst.SST.data)
-    xco2_images = preprocess_image_reduced(xco2.XCO2.data,xco2=True)
-    
-    pco2_images = preprocess_image_reduced(pco2.pCO2.data)
+    else:
+        chl_images = preprocess_image_reduced(chl.Chl.data)
+        mld_images = preprocess_image_reduced(mld.MLD.data)
+        sss_images = preprocess_image_reduced(sss.SSS.data)
+        sst_images = preprocess_image_reduced(sst.SST.data)
+        xco2_images = preprocess_image_reduced(xco2.XCO2.data,xco2=True)
+        pco2_images = preprocess_image_reduced(pco2.pCO2.data)
     
     X = np.dstack((chl_images, mld_images, sss_images, sst_images, xco2_images))
     X = X.reshape((421,180,360,5),order='F')
 
     return X, pco2_images
 
-def preprocess_images_nfp(dir_name):
+def preprocess_images_nfp(dir_name,num="001",socat=False):
     
-    chl,mld,sss,sst,u10,fg_co2,xco2,icefrac,patm,pco2 = read_xarray(dir_name)
+    chl,mld,sss,sst,u10,fg_co2,xco2,icefrac,patm,pco2 = read_xarray(dir_name,num)
     
-    chl_images = preprocess_image_reduced(chl.Chl.data)
-    mld_images = preprocess_image_reduced(mld.MLD.data)
-    sss_images = preprocess_image_reduced(sss.SSS.data)
-    sst_images = preprocess_image_reduced(sst.SST.data)
-    xco2_images = preprocess_image_reduced(xco2.XCO2.data,xco2=True)
-    
-    pco2_images = preprocess_image_reduced(pco2.pCO2.data)
+    if socat:
+
+        chl_images = preprocess_image_reduced(chl.Chl_socat.data)
+        mld_images = preprocess_image_reduced(mld.MLD_socat.data)
+        sss_images = preprocess_image_reduced(sss.SSS_socat.data)
+        sst_images = preprocess_image_reduced(sst.SST_socat.data)
+        xco2_images = preprocess_image_reduced(xco2.XCO2.data,xco2=True)
+        pco2_images = preprocess_image_reduced(pco2.pCO2_socat.data)
+    else:
+        chl_images = preprocess_image_reduced(chl.Chl.data)
+        mld_images = preprocess_image_reduced(mld.MLD.data)
+        sss_images = preprocess_image_reduced(sss.SSS.data)
+        sst_images = preprocess_image_reduced(sst.SST.data)
+        xco2_images = preprocess_image_reduced(xco2.XCO2.data,xco2=True)
+        pco2_images = preprocess_image_reduced(pco2.pCO2.data)
     
     X = np.dstack((chl_images, mld_images, sss_images, sst_images, xco2_images,pco2_images))
     X = X.reshape((421,180,360,6),order='F')
 
     return X, pco2_images
-
 
 
 def preprocess_image_reduced(data,xco2=False):
